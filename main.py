95877c47eced7b8877320079d488cbc643f6915a
Josefine Becker <josefine2468@gmail.com>
2024-01-10 19:25:39 +0100

    mini changes

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from visuals import *
     m:              fuzzy factor > 1 
     metric:         distance metric as lambda expression
     epsilon:        error value > 0 to terminate
-    init:           way of initializing the u matrix => currently not used (=random)
     max_iterations: maximum count of possible iterations before terminating
+    make_visuals:   create jpeg images for all dimensions
 """
 def fuzzyCMeans(points, cluster_count, m, metric=(lambda x, y: sum([(a - b) ** 2 for a, b in zip(x, y)])),
-                epsilon=0.001, init=None, max_iterations=100):
+                epsilon=0.001, max_iterations=100, make_visuals=True):
     n = len(points)
     v = np.zeros([cluster_count, len(points[0])])
     u = np.zeros([cluster_count, n])
@@ -50,28 +50,33 @@ def fuzzyCMeans(points, cluster_count, m, metric=(lambda x, y: sum([(a - b) ** 2
                 sum = 0
                 for j in range(cluster_count):
                     sum += (dik / metric(points[k], v[j])) ** (2 / m - 1)
-                u[i][k] = 1 / sum
+                if(0==sum): 
+                    u[i][k] = 1
+                else: 
+                    u[i][k] = 1 / sum
         iteration += 1
 
-    cluster_no = np.argmax(u, axis=0)
+    label = np.argmax(u, axis=0)
     probabilities = np.max(u, axis=0)
 
-    links = makePlots(points, v, probabilities, cluster_no)
-    return points, cluster_no, links
+    if(make_visuals):
+        links = makePlots(points, v, probabilities, label)
+        return label, links
+    return label, []
 
 
 # ---- Beispieltests ---- #
 
 if __name__ == '__main__':
     different_metrics = {
-        "squared_eucledian": lambda x, y: sum([(a - b) ** 2 for a, b in zip(x, y)]),
-        "eucledian": lambda x, y: sum([(a - b) ** 2 for a, b in zip(x, y)]) ** (1 / 2),
+        "squared_euclidean": lambda x, y: sum([(a - b) ** 2 for a, b in zip(x, y)]),
+        "euclidean": lambda x, y: sum([(a - b) ** 2 for a, b in zip(x, y)]) ** (1 / 2),
         "manhattan": lambda x, y: sum([abs(a - b) for a, b in zip(x, y)]),
-        "inverse_eucleadian": lambda x, y: 1 / (sum([(a - b) ** 2 for a, b in zip(x, y)]) ** (1 / 2))
+        "inverse_euclidean": lambda x, y: 1 / (sum([(a - b) ** 2 for a, b in zip(x, y)]) ** (1 / 2))
     }
 
     # example call of function
     wine_data = load_wine()
-    inData = np.array(wine_data.get("data"))
+    in_data = np.array(wine_data.get("data"))
     # ATTENTION: Wine dataset has 13 Dimensions => many 13*12 pictures will be generated #
-    fuzzyCMeans(inData, cluster_count=3, m=1.1, epsilon=0.01)
+    fuzzyCMeans(in_data, cluster_count=3, m=1.1, epsilon=0.01, make_visuals=True)
